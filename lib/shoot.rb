d8eb4e051be50a0aaf6288c365cbb773fd9c2409
Juan Lulkin <juan.lulkin@klarna.com>
2015-06-03 14:02:37 +0300

    Adds autoload for ngrok and ngrok_pow

--- lib/shoot.rb
+++ lib/shoot.rb
@@ -1,5 +1,7 @@
 module Shoot
   autoload :CLI, "shoot/cli"
   autoload :Scenario, "shoot/scenario"
+  autoload :Ngrok, "shoot/ngrok"
+  autoload :NgrokPow, "shoot/ngrok_pow"
   autoload :VERSION, "shoot/version"
 end
