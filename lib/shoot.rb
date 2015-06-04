ee4b7d7c41626cd73107a6d072be62a8e88c965d
Juan Lulkin <juan.lulkin@klarna.com>
2015-06-04 23:38:50 +0300

    Adds new autoloaders, aligns and sorts

--- lib/shoot.rb
+++ lib/shoot.rb
@@ -1,7 +1,10 @@
 module Shoot
-  autoload :CLI, "shoot/cli"
-  autoload :Scenario, "shoot/scenario"
-  autoload :Ngrok, "shoot/ngrok"
-  autoload :NgrokPow, "shoot/ngrok_pow"
-  autoload :VERSION, "shoot/version"
+  autoload :Browser,        "shoot/browser"
+  autoload :CLI,            "shoot/cli"
+  autoload :Ngrok,          "shoot/ngrok"
+  autoload :NgrokPow,       "shoot/ngrok_pow"
+  autoload :Scenario,       "shoot/scenario"
+  autoload :ScenarioRunner, "shoot/scenario_runner"
+  autoload :UI,             "shoot/ui"
+  autoload :VERSION,        "shoot/version"
 end
